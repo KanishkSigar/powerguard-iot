3f5d091079bf4ee86938f67df100ca75312516e8
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:37 +0530

    feat(frontend): implement TextInput UI component class (21/42)

---
+++ frontend/js/components/TextInput.js
@@ -0,0 +1,10 @@
+﻿export class TextInput {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'TextInput'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
