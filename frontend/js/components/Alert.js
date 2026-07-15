087ea7fa985d0ab663dbd63b1306d3c47336ad88
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Alert UI component class (10/42)

---
+++ frontend/js/components/Alert.js
@@ -0,0 +1,10 @@
+﻿export class Alert {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Alert'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
