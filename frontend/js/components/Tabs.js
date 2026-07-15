99481d84648a416f55b67d76bce3f5974a4bb7f0
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Tabs UI component class (13/42)

---
+++ frontend/js/components/Tabs.js
@@ -0,0 +1,10 @@
+﻿export class Tabs {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Tabs'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
