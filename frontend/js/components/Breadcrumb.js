c5c187bf688ba8c347404d5885a4e33777065cdf
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Breadcrumb UI component class (15/42)

---
+++ frontend/js/components/Breadcrumb.js
@@ -0,0 +1,10 @@
+﻿export class Breadcrumb {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Breadcrumb'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
