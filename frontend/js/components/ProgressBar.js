5501ea38c8650221a2045c958e322f8f9c4db948
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:37 +0530

    feat(frontend): implement ProgressBar UI component class (16/42)

---
+++ frontend/js/components/ProgressBar.js
@@ -0,0 +1,10 @@
+﻿export class ProgressBar {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'ProgressBar'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
