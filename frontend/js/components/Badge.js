32b3cc9c526349972c70b429a3bf5b99f9f30bb2
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Badge UI component class (8/42)

---
+++ frontend/js/components/Badge.js
@@ -0,0 +1,10 @@
+﻿export class Badge {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Badge'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
