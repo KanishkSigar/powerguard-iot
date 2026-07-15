5aac62ae461f84cb917e412c5d1375ef96f4fdfb
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Dropdown UI component class (5/42)

---
+++ frontend/js/components/Dropdown.js
@@ -0,0 +1,10 @@
+﻿export class Dropdown {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Dropdown'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
