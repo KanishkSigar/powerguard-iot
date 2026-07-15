437ded0baa20eab6c8a7f46d7d7671365f17389c
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Accordion UI component class (12/42)

---
+++ frontend/js/components/Accordion.js
@@ -0,0 +1,10 @@
+﻿export class Accordion {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Accordion'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
