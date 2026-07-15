7ea001c5c092a7469711de4fe36169b73771f784
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:37 +0530

    feat(frontend): implement Slider UI component class (17/42)

---
+++ frontend/js/components/Slider.js
@@ -0,0 +1,10 @@
+﻿export class Slider {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Slider'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
