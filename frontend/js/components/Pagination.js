254d80f067640bb5701b7874ce776827503c5e96
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Pagination UI component class (14/42)

---
+++ frontend/js/components/Pagination.js
@@ -0,0 +1,10 @@
+﻿export class Pagination {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Pagination'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
