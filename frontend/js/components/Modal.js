f626769eacf6cdcfb5cb48e1041b809456fe4338
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Modal UI component class (3/42)

---
+++ frontend/js/components/Modal.js
@@ -0,0 +1,10 @@
+﻿export class Modal {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Modal'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
