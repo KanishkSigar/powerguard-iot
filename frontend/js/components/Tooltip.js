ca4c9ddcb7fc1af27bd0fe9987879f5fea858636
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Tooltip UI component class (4/42)

---
+++ frontend/js/components/Tooltip.js
@@ -0,0 +1,10 @@
+﻿export class Tooltip {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Tooltip'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
