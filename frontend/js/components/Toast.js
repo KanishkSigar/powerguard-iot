d7ff3ca852a397dce926a1498e089d93730fe13e
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Toast UI component class (11/42)

---
+++ frontend/js/components/Toast.js
@@ -0,0 +1,10 @@
+﻿export class Toast {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Toast'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
