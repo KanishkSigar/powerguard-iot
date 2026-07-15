db28b28827145d75910ec841d080d467ab44335e
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:34 +0530

    feat(frontend): implement Button UI component class (1/42)

---
+++ frontend/js/components/Button.js
@@ -0,0 +1,10 @@
+﻿export class Button {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Button'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
