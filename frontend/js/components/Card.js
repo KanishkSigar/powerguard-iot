dd5b9fe446515c02808aed033b873c7d750c1663
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Card UI component class (2/42)

---
+++ frontend/js/components/Card.js
@@ -0,0 +1,10 @@
+﻿export class Card {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Card'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
