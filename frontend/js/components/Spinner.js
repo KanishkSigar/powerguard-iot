a153c8d885ca106d52f43583f48d46600dd93f8b
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:36 +0530

    feat(frontend): implement Spinner UI component class (9/42)

---
+++ frontend/js/components/Spinner.js
@@ -0,0 +1,10 @@
+﻿export class Spinner {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Spinner'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
