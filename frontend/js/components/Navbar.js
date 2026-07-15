974408fe0085544a19d8b042f4cb80da3f3683f4
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Navbar UI component class (6/42)

---
+++ frontend/js/components/Navbar.js
@@ -0,0 +1,10 @@
+﻿export class Navbar {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Navbar'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
