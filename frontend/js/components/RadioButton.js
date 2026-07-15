62808baf8f32159f2eb3faadbb5d846cea8e7f69
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:37 +0530

    feat(frontend): implement RadioButton UI component class (20/42)

---
+++ frontend/js/components/RadioButton.js
@@ -0,0 +1,10 @@
+﻿export class RadioButton {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'RadioButton'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
