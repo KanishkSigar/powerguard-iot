f7e2653bb466af8ad83919c5b5af0ac39cbc21db
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:35 +0530

    feat(frontend): implement Sidebar UI component class (7/42)

---
+++ frontend/js/components/Sidebar.js
@@ -0,0 +1,10 @@
+﻿export class Sidebar {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Sidebar'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
