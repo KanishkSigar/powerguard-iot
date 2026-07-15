654db2bdb044adad34fab32a5c7c920cb79befd2
KanishkSigar <kanishksigar@gmail.com>
2026-07-15 13:54:37 +0530

    feat(frontend): implement Checkbox UI component class (19/42)

---
+++ frontend/js/components/Checkbox.js
@@ -0,0 +1,10 @@
+﻿export class Checkbox {
+    constructor() {
+        this.element = document.createElement('div');
+        this.element.className = 'Checkbox'.toLowerCase();
+    }
+    render() {
+        return this.element;
+    }
+}
+
